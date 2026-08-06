71f9b3670737de84b2ccfbd6f399336d9e5d54b4
tsarevkostya03-maker <tsarevkostya03@gmail.com>
2026-08-06 23:22:59 +0300

    Update DeliveryTest.java

--- src/test/java/ru/netology/delivery/test/DeliveryTest.java
+++ src/test/java/ru/netology/delivery/test/DeliveryTest.java
@@ -1,9 +1,10 @@
-package ru.netology.delivery;
+package ru.netology.delivery.test;
 
 import com.codeborne.selenide.Condition;
 import com.codeborne.selenide.Configuration;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import ru.netology.delivery.data.DataGenerator;
 
 import java.time.Duration;
 
